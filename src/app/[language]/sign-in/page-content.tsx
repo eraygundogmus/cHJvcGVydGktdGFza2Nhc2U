5d801c7561dd410c03496cf2262e79157c447672
eraygundogmus <eray.gundogmus@akinon.com>
2024-04-19 21:47:09 +0300

    feat: finalize sign in page UI

--- src/app/[language]/sign-in/page-content.tsx
+++ src/app/[language]/sign-in/page-content.tsx
@@ -2,7 +2,7 @@
 import { useTranslation } from "@/services/i18n/client";
 import { useRouter } from "next/navigation";
 import { signInWithGoogle } from "@/lib/firebase/auth";
-
+import { Button } from "@/components/ui/button";
 export default function SignIn() {
   const router = useRouter();
   const { t } = useTranslation("sign-in");
@@ -13,14 +13,15 @@ export default function SignIn() {
     if (isOk) router.push("/");
   };
 
-  const buttonStyle = "bg-slate-500 mt-2 px-2 py-1 rounded-md text-slate-50";
-
   return (
-    <>
-      <h1>{t("title")}</h1>
-      <button className={buttonStyle} onClick={handleSignIn}>
-        {t("signInWithGoogle")}
-      </button>
-    </>
+    <div className="mt-10">
+      <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl">
+        {t("title")}{" "}
+      </h1>
+      <h2 className="my-6 w-1/2 text-neutral-700 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+        {t("description")}
+      </h2>
+      <Button onClick={handleSignIn}>{t("signInWithGoogle")}</Button>
+    </div>
   );
 }
