b50302e1fbe5b7086b680f9c91cd99a290cd4cd2
eraygundogmus <eray.gundogmus@akinon.com>
2024-04-19 21:06:35 +0300

    feat: add storeLanguageProvider to the layout and generate meta data with i18n

--- src/app/[language]/layout.tsx
+++ src/app/[language]/layout.tsx
@@ -2,31 +2,50 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "../globals.css";
 import { cn } from "@/lib/utils";
+import { dir } from "i18next";
+import "@/services/i18n/config";
+import { getServerTranslation } from "@/services/i18n";
+import StoreLanguageProvider from "@/services/i18n/store-language-provider";
+import { languages } from "@/services/i18n/config";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
-export const metadata: Metadata = {
-  title: "Roman Numeral Calculator",
-  description: "A simple calculator for Roman numerals.",
+type Props = {
+  params: { language: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { t } = await getServerTranslation(params.language, "common");
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export function generateStaticParams() {
+  return languages.map((language) => ({ language }));
+}
 
 export default function RootLayout({
   children,
+  params: { language },
 }: Readonly<{
   children: React.ReactNode;
+  params: { language: string };
 }>) {
   return (
-    <html lang="en">
+    <html lang={language} dir={dir(language)}>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        {children}
+        <StoreLanguageProvider>{children}</StoreLanguageProvider>
       </body>
     </html>
   );
