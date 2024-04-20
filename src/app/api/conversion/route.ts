dc4f4e6c3ca3b89e0ffeba38dd5870137ed76699
eraygundogmus <eray.gundogmus@akinon.com>
2024-04-20 12:52:53 +0300

    feat: add rate limiter for conversions

--- src/app/api/conversion/route.ts
+++ src/app/api/conversion/route.ts
@@ -1,17 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import { RomanConverter } from "@/lib/converter";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 export interface ConversionResponse {
-  convertedNumber: number;
+  result: string;
 }
+
+const rateLimiter = new RateLimiterMemory({
+  points: 15,
+  duration: 60,
+});
 
 export async function POST(
   req: NextRequest
 ): Promise<NextResponse<ConversionResponse | any>> {
-  const body = await req.json();
-  const number = body.number;
+  try {
+    await rateLimiter.consume(req.ip);
 
-  return NextResponse.json({
-    result: RomanConverter.toRoman(number),
-  });
+    const body = await req.json();
+    const number = body.number;
+
+    return NextResponse.json({
+      result: RomanConverter.toRoman(number),
+    });
+  } catch (rateLimiterError) {
+    // Handling rate limiter error
+    return new NextResponse(
+      JSON.stringify({
+        error: "Too many requests, please try again later.",
+      }),
+      {
+        status: 429,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
